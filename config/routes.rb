04daad20eacd68f0ce048c712a1ea7d27da93281
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-06 15:54:54 -0700

    add routes for bands, albums, tracks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: redirect('/users/1')
 
-  resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create, :new, :show]
+  resource :session, only: [:create, :new, :destroy]
+
+  resources :bands
+  resources :albums, :tracks, only: [:create, :new, :edit, :show, :update, :destroy]
 end
