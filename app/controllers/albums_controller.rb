209a54dfd37633dba2ac8aae492b6653ab4e9ba5
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-06 17:14:38 -0700

    add views to bands routes

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,21 @@
 class AlbumsController < ApplicationController
+  before_action :require_login
+  
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   helper_method :current_user
 
@@ -28,5 +28,9 @@ class ApplicationController < ActionController::Base
   private
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def require_login
+    redirect_to new_session_url unless logged_in?
   end
 end
